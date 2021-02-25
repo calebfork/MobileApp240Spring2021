1eb5ed2062df846afaaafd1f31a087cfa9b89cb1
cw-sys <73605397+cw-sys@users.noreply.github.com>
2021-02-25 09:39:31 -0600

    Add files via upload
    update for html,css,js; atm no php,imgs,js_logic/magic

---
+++ MobileApp240Spring2021-main/README.md
@@ -0,0 +1,2 @@
+# MobileApp240Spring2021
+ CIS-240 Spring2021 MobileApp
---
+++ MobileApp240Spring2021-main/regis_rex_mein_main/js/main.js
@@ -0,0 +1,53 @@
+//js main file
+
+function button_alert() {//needs $php call
+    window.alert("Here is a page response example");
+}
+
+
+var window_object_reference;
+var window_features = "menubar=no,location=no,resizable=no,scrollbars=no,status=no";
+
+function open_roll_popup() {//pop-out window generation
+    window_object_reference = window.open("https://www.cnn.com/", "CNN_WindowName", window_features)
+}
+
+
+
+
+
+
+//
+//
+//archaic js
+/*var navbar = '<div class="btn-toggle-nav" onclick="navButton()">|||</div>\
+	<ul>\
+		<li><a href="careers.html">Careers</a></li>\
+		<li><a href="downloadpage.html">Downloads</a></li>\
+		<li><a href ="about.html">About</a></li>\
+		<li><a href="index.html">Home</a>\
+			<ul><a href="index.html">submenu1</a></ul>\
+			<ul><a href="index.html">submenu2</a></ul>\
+			<ul><a href="index.html">submenu3</a></ul>\
+		</li>\
+	</ul>';
+
+function loadNav() {//loads navbar
+	document.getElementById("nav").innerHTML = navbar;
+}
+
+var foot1 = '<footer>\
+   <p>\
+      Ozarks Technical Community College 2020. Unless otherwise \
+      indicated, all content on this web site is licensed under a \
+      Creative Commons License.**\
+   </p>\
+</footer>';
+
+function loadFoot() {//loads footer
+	document.getElementById("foot1").innerHTML = foot1;
+}
+
+function navButton(){
+	let x = document.getElementById("");
+}*/
